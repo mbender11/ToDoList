beb1ab048a693f32150d92a3064b6ba8f5d88265
Mason Bender <bender_mason@yahoo.com>
2018-02-21 13:49:42 -0700

    Test of xcode source control

--- ToDoList/FirstViewController.swift
+++ ToDoList/FirstViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    // Testing git
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
